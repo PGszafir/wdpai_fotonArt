b8e8bac622ef469e90ea19caf2490bc8df128b3a
PGszafir <pgszafir@gmail.com>
2021-11-17 11:11:08 +0200

    uporzadkowanie projektu

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
-
-echo 'Hi there 👋';
+require 'Routing.php';
+//echo 'Hi there 👋';
+$path = $_SERVER['REQUEST_URI'];
+$path = trim($path,'/');
+$path = parse_url($path,PHP_URL_PATH);
+Router::run($path);
